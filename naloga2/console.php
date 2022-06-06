6aef407737d3cf04e1ae8e4f47b52ed04f001098
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-06 19:41:35 +0200

    loga 2 is done

--- naloga2/console.php
+++ naloga2/console.php
@@ -1,8 +1,5 @@
 <?php
 
-$json = file_get_contents('response.json');
-
+$json = file_get_contents("https://api.coinbase.com/v2/prices/BTC-USD/spot");
 $json_data = json_decode($json, true);
-
-//sort($json_data);
-print_r($json_data);
+echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
