37c6220824f0be325452e27d58904b751725bffb
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-09 17:38:42 +0200

    pol narejena 3 naloga

--- naloga2/console.php
+++ naloga2/console.php
@@ -1,5 +1,10 @@
 <?php
 
-$json = file_get_contents("https://api.coinbase.com/v2/prices/BTC-USD/spot");
+$currency = readline('Enter a string:');
+
+
+$json = file_get_contents("https://api.coinbase.com/v2/prices/$currency-USD/spot");
 $json_data = json_decode($json, true);
+
+
 echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
