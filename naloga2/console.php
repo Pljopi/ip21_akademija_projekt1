16a97814573bb1868e6f3f03b0063b42ae55cad1
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-09 20:59:06 +0200

    Naloga 3 done

--- naloga2/console.php
+++ naloga2/console.php
@@ -3,13 +3,12 @@
 $currency = readline('Enter a string:');
 
 if ($currency == '') {
-    echo  "When propmted to enter a string, enter a crypto currency tag... like BTC or ETH  ";
-} else if ($currency = "help") {
-    echo "When propmted too enter a string, enter a cryptocurrency TAG, try BTC";
+    echo  "When propmted to, enter a crypto currency tag... try again... ";
+} else if ($currency == "help") {
+    echo "When propmted to, enter a crypto currency tag like BTC, ETH, ...";
 } else {
     $json = file_get_contents("https://api.coinbase.com/v2/prices/$currency-USD/spot");
     $json_data = json_decode($json, true);
-
 
 
     echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
