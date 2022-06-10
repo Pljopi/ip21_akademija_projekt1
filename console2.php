2baeb67ecf45d16f05bdc2a1a04027d51961caf5
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-10 19:27:25 +0200

    console2.php 1/2 naloge 4 narejena

--- console2.php
+++ console2.php
@@ -3,11 +3,15 @@
 $criptocurrency = $argv[1];
 $currency = $argv[2];
 
+$lenghtcurrency = strlen($currency);
+$lenghtcriptocurrency = strlen($criptocurrency);
+
 if (($currency == '') or ($criptocurrency == '')) {
     echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
 } else if ($currency == "help" or $criptocurrency == "help") {
-    echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
-} else {
+    echo "After file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
+} else if ($lenghtcriptocurrency or $currency > 5) {
+    echo "Currency and criptocurrency TAG cannot be longer than 5 characters";
     $json = file_get_contents("https://api.coinbase.com/v2/prices/$criptocurrency-$currency/spot");
     $json_data = json_decode($json, true);
     echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
