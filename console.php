611799169e74b1663edbd3600d429581a9e9e062
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-10 18:48:38 +0200

    Pravilno poimenoval spremenljivke v console.php in uporabil argv za doseči isto v console2.php

--- console.php
+++ console.php
@@ -1,13 +1,14 @@
 <?php
 
-$currency = readline('enter a cryptocurrency TAG:');
+$cryptocurrency = readline('enter a cryptocurrency TAG:');
+$currency = readline('enter a currency TAG:');
 
-if ($currency == '') {
-    echo  "When propmted to, enter a crypto currency tag... try again... ";
+if ($currency == '' or $cryptocurrency == '') {
+    echo  "When propmted to, enter a crypto currency tag, or currency tag... try again... ";
 } else if ($currency == "help") {
     echo "When propmted to, enter a crypto currency tag like BTC, ETH, ...";
 } else {
-    $json = file_get_contents("https://api.coinbase.com/v2/prices/$currency-USD/spot");
+    $json = file_get_contents("https://api.coinbase.com/v2/prices/$cryptocurrency-$currency/spot");
     $json_data = json_decode($json, true);
 
 
---
+++ console2.php
@@ -0,0 +1,12 @@
+<?php
+
+$criptocurrency = $argv[1];
+$currency = $argv[2];
+
+if (($currency == '') or ($criptocurrency == '')) {
+    echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
+} else {
+    $json = file_get_contents("https://api.coinbase.com/v2/prices/$criptocurrency-$currency/spot");
+    $json_data = json_decode($json, true);
+    echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
+}
