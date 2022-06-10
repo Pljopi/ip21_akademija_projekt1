2e4bf215353d3d634a9411116285970b18f6560d
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-10 18:53:30 +0200

    zdej deluje help -.-''

--- console2.php
+++ console2.php
@@ -5,7 +5,7 @@ $currency = $argv[2];
 
 if (($currency == '') or ($criptocurrency == '')) {
     echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
-} else if ($currency == help or $criptocurrency == help) {
+} else if ($currency == "help" or $criptocurrency == "help") {
     echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
 } else {
     $json = file_get_contents("https://api.coinbase.com/v2/prices/$criptocurrency-$currency/spot");
