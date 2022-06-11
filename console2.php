b5be76739725211218992e54ad7351f8dfa0be35
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-11 17:47:56 +0200

    Naloga 4 done

--- console2.php
+++ console2.php
@@ -1,19 +1,26 @@
 <?php
+set_error_handler("errorHandler", E_WARNING);
+function errorHandler(
+    int $type,
+    string $msg,
+    ?string $file = null,
+    ?int $line = null
+) {
+    echo " Your input is incorrect, after file name enter cryptocurrency TAG, space, currency TAG"; //$type . " " . $msg . " " . " in " . $file . " on line " . $line . "BLYAT";
+    exit;
+}
 
 $criptocurrency = $argv[1];
 $currency = $argv[2];
-
-$lenghtcurrency = strlen($currency);
-
 
 if ((!$currency) or (!$criptocurrency)) {
     echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
 } else if ($currency == "help" || $criptocurrency == "help") {
     echo "After file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
-} else if ($lenghtcriptocurrency > 5 || strlen($criptocurrency) > 5) {
+} else if (strlen($currency) > 5 || strlen($criptocurrency) > 5) {
     echo "Currency and criptocurrency TAG cannot be longer than 5 characters";
 } else {
-    $json = file_get_contents("https://api.coinbase.com/v2/prices/$criptocurrency-$currency/spot");
+    $json = file_get_contents(sprintf("https://api.coinbase.com/v2/prices/%s-%s/spot", $criptocurrency, $currency));
     $json_data = json_decode($json, true);
     echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
 }
