3b3a1ca1206fb0f1d6442707f2e9ba4974f075f6
YoMamaisAniceLady <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-06-11 20:04:27 +0200

    Naloga 5 done

--- console2.php
+++ console2.php
@@ -1,4 +1,5 @@
 <?php
+//error handler
 set_error_handler("errorHandler", E_WARNING);
 function errorHandler()
 {
@@ -6,17 +7,46 @@ function errorHandler()
     exit;
 }
 
+//spremenljivke
 $criptocurrency = $argv[1];
 $currency = $argv[2];
+$jsonALL = file_get_contents("https://api.coingecko.com/api/v3/simple/supported_vs_currencies"); //ta je za listo valut katerih pari so podprti
+$jsonALL_data = json_decode($jsonALL, true);
 
-if ((!$currency) or (!$criptocurrency)) {
-    echo "after file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
+//išče user input fiat v listi podprtih parov
+function currency($currency, $jsonALL_data)
+{
+    foreach ($jsonALL_data as $value) {
+        if ("$currency" == $value) {
+            return true;
+        }
+    }
+}
+//išče user input crypto v listi podprtih parov
+function criptocurrency($criptocurrency, $jsonALL_data)
+{
+    foreach ($jsonALL_data as $value) {
+        if ("$criptocurrency" == $value) {
+            return true;
+        }
+    }
+}
+
+//izpis cene poljubnega para
+function pairANY($criptocurrency, $currency,)
+{
+    $json = file_get_contents(sprintf("https://api.coinbase.com/v2/prices/%s-%s/spot", $criptocurrency, $currency));
+    $json_data = json_decode($json, true);
+    echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"]; //ta je za pair price
+
+}
+//če je vse ok se izpelje funkcija
+if (currency($currency, $jsonALL_data) == true && criptocurrency($criptocurrency, $jsonALL_data) == true) {
+    pairANY($criptocurrency, $currency);
 } else if ($currency == "help" || $criptocurrency == "help") {
-    echo "After file name input criptocurrency TAG sapce currency TAG of the pair you would like to see";
+    echo "After file name input criptocurrency TAG sapce currency TAG of the pair you would like to see, both in lower case letters";
 } else if (strlen($currency) > 5 || strlen($criptocurrency) > 5) {
     echo "Currency and criptocurrency TAG cannot be longer than 5 characters";
 } else {
-    $json = file_get_contents(sprintf("https://api.coinbase.com/v2/prices/%s-%s/spot", $criptocurrency, $currency));
-    $json_data = json_decode($json, true);
-    echo "Spot price of " . $json_data["data"]["base"] . " is " . $json_data["data"]["amount"] . " " . $json_data["data"]["currency"];
+    echo "Error, the currency and/or criptocurrency TAG you have entered does not exist, make sure that both TAGs are lower case. ";
 }
