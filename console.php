3487531ba023f072a9a1eceda4fed5be33ed323a
YoMamaisAniceLAdy <andrejcepak@gmail.com>
2022-07-08 20:44:23 +0200

    faza 2

--- console.php
+++ console.php
@@ -1,6 +1,8 @@
 <?php
+require_once '/home/andrej/ip21_akademija_projekt1/lib/model.php';
 //Set variable as decoded array of supported currenices, got from the apiCall function
 $listOfSupportedCurrencies = apiCall("https://api.coingecko.com/api/v3/simple/supported_vs_currencies");
+
 //if no user input echo Help, and exit.
 if (!isset($argv[1])) {
     helpTxt();
@@ -51,26 +53,7 @@ function helpTxt()
 }
 
 //Call api function for currencies list and price pair value
-function apiCall($pricePairOrUrlPair)
-{
-    $ch = curl_init();
-    curl_setopt_array($ch, array(
-        CURLOPT_URL => $pricePairOrUrlPair,
-        CURLOPT_RETURNTRANSFER => 1,
-    ));
-    $curl_data = curl_exec($ch);
-    $httpCode = (curl_getinfo($ch, CURLINFO_HTTP_CODE));
-    //Checks that the url request has succeded, otherwise it ends execution and echoes message for user
-    if ($httpCode !== 200) {
-        throw new \Exception("You have entered a unsupported currency pair, or api is down\n");
-        return;
-    }
 
-    return json_decode($curl_data, true);
-
-    curl_close($ch);
-
-}
 //Sorts currencies list by alphabet order and breaks line after each one.Ease of read only.
 function printList($listOfSupportedCurrencies)
 {
@@ -97,20 +80,7 @@ function ifEmptyOrLong($argv)
     }
 }
 //Get's price pair value and echoes it to the user
-function getPrice($criptoCurrency, $currency)
-{
 
-    $pricePairUrl = sprintf("https://api.coinbase.com/v2/prices/%s-%s/spot", $criptoCurrency, $currency);
-    $pricePairUrlApiCall = apiCall($pricePairUrl);
-    $pricePair = [
-        $pricePairUrlApiCall["data"]["base"],
-        $pricePairUrlApiCall["data"]["amount"],
-        $pricePairUrlApiCall["data"]["currency"],
-    ];
-
-    echoPrice($pricePair[0], $pricePair[1], $pricePair[2]);
-
-}
 //Echoes price pair, uses $pricePair array values 0-2 as parameters (from inside getPrice function)
 
 function echoPrice($criptoCurrencyTAG, $pairValue, $currencyTAG)
