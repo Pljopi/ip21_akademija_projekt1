26cd1d74bec49e2fbf9ad3ff957015b8e9400b4c
YoMamaisAniceLAdy <andrejcepak@gmail.com>
2022-07-10 15:31:19 +0200

    Popravki done

--- lib/model.php
+++ lib/model.php
@@ -1,25 +1,34 @@
 <?php
-
+//sets variable with api url with list of all supported currencies
+$getList = apiCall("https://api.coingecko.com/api/v3/simple/supported_vs_currencies");
 //Call api function for currencies list and price pair value
 function apiCall($pricePairOrUrlPair)
 {
-    $ch = curl_init();
-    curl_setopt_array($ch, array(
-        CURLOPT_URL => $pricePairOrUrlPair,
-        CURLOPT_RETURNTRANSFER => 1,
-    ));
-    $curl_data = curl_exec($ch);
-    $httpCode = (curl_getinfo($ch, CURLINFO_HTTP_CODE));
-    //Checks that the url request has succeded, otherwise it ends execution and echoes message for user
-    if ($httpCode !== 200) {
-        throw new \Exception("You have entered a unsupported currency pair, or api is down\n");
-        return;
+    try {
+        $ch = curl_init();
+        curl_setopt_array($ch, array(
+            CURLOPT_URL => $pricePairOrUrlPair,
+            CURLOPT_RETURNTRANSFER => 1,
+        ));
+        $curl_data = curl_exec($ch);
+        $httpCode = (curl_getinfo($ch, CURLINFO_HTTP_CODE));
+        //Checks that the url request has succeded, otherwise it ends execution and echoes message for user
+        if (json_decode($curl_data, true) === null || json_decode($curl_data, true) === false) {
+            throw new \Exception("Api is down\n");
+
+        }
+        if ($httpCode !== 200) {
+            throw new \Exception("You have entered a unsupported currency pair\n");
+
+        }
+        curl_close($ch);
+
+        return json_decode($curl_data, true);
+
+    } catch (\Exception$e) { //Catches exceptions, and returns the exception msg.
+        echo $e->getMessage();
+        exit; //without this exit, function keeps executing
     }
-
-    return json_decode($curl_data, true);
-
-    curl_close($ch);
-
 }
 
 //Get's price pair value and echoes it to the user
@@ -33,8 +42,6 @@ function getPrice($criptoCurrency, $currency)
         $pricePairUrlApiCall["data"]["amount"],
         $pricePairUrlApiCall["data"]["currency"],
     ];
-
-    echoPrice($pricePair[0], $pricePair[1], $pricePair[2]);
-
+    return [$pricePair[0], $pricePair[1], $pricePair[2]];
 }
 //Echoes price pair, uses $pricePair array values 0-2 as parameters (from inside getPrice function)
--- lib/views/consoleView.php
+++ lib/views/consoleView.php
@@ -1,1 +1,22 @@
+<?php
+//Echoes price pair, uses $pricePair array values 0-2 as parameters (from inside getPrice function)
+function echoPrice($criptoCurrencyTAG, $pairValue, $currencyTAG)
+{
 
+    echo sprintf("Spot price of %s is %s %s\n", $criptoCurrencyTAG, $pairValue, $currencyTAG);
+    return;
+}
+
+function printList($listOfSupportedCurrencies)
+{
+    natcasesort($listOfSupportedCurrencies);
+    foreach ($listOfSupportedCurrencies as $value) {
+        echo "$value \n";
+    }
+}
+
+function helpTxt()
+{
+    echo "Help -\n After file name input either >list< to get a list of supported currencies\nor\n>price< >FiatCurrency< TAG >criptoCurrency TAG< \n";
+
+}
