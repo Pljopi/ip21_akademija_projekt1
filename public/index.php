262438a5f3fe6343be8492492c4ff8919097fad0
Andrej Cepak <105911557+YoMamaisAniceLady@users.noreply.github.com>
2022-08-09 18:17:12 +0200

    Delete index.php

--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '/var/www/projekt1/lib/bootstrap/bootstrap.php';
-require_once '/var/www/projekt1/lib/model.php';
-$model = new Model();
-$list = $model->getList();
-$printFav = $model->printFav();
-echo ($twig->render('list.html.twig', ['ListOfCurrencies' => $list]));
