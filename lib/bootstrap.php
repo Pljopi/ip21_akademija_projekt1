5c71c198624568d116d79b76ac9e00058ddff36a
Andrej Cepak <andrejcepak@gmail.com>
2022-08-25 15:26:05 +0200

    reapired the deleted _ in bootstrap.php line 12

--- lib/bootstrap.php
+++ lib/bootstrap.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/model.php';
 $model = new Model();
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(_DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/../');
 $dotenv->load();
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/views');
 $twig = new \Twig\Environment($loader, [  ]);
